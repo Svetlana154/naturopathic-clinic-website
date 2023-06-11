3c5783903cf1f4330af53a185a18685c57aeef21
Svetlana154 <svetlana.esina.360@gmail.com>
2023-06-11 12:51:54 -0400

    Added initial booking page

--- scripts/general.js
+++ scripts/general.js
@@ -1,6 +1,1 @@
-// enabling tooltips on page
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-$(document).ready(function() {
-    $("body").tooltip({ selector: '[data-bs-toggle="tooltip"]' });
-});
+
